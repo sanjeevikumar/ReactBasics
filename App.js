b3729be2c023ca434564d858637910459c1c373f
sanjeevikumar <sanjaysridhar0995@gmail.com>
2024-02-13 22:07:21 +0530

    Namaste React EP-01

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // React.createElement/ReactDOM.createRoot React element is the object that becomes html that the browser understands
-// "h1" tag in index.html will be replaced by heading variable element from App.js.
+// "h1" tag in index.html will be replaced by heading variable element from App.js
 const heading = React.createElement("h1", {}, "Hello sanjay");
 const tag = ReactDOM.createRoot(document.getElementById("root"));
 tag.render(heading);
