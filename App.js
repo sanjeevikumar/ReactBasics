daaa77b6ac1c504bda1185d00240fc469a6200a8
sanjeevikumar <sanjaysridhar0995@gmail.com>
2024-02-20 23:01:09 +0530

    episode 2 and episode 3 change

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 // React.createElement/ReactDOM.createRoot React element is the object that becomes html that the browser understands
-// "h1" tag in index.html will be replaced by heading variable element from App.js
-const heading = React.createElement("h1", {}, "Hello sanjay");
-const tag = ReactDOM.createRoot(document.getElementById("root"));
-tag.render(heading);
+// "h1" tag in index.html will be replaced by heading variable element from App.js.
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React Element
+const heading = <h1 className="remote">Welcome to react basics</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
